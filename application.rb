020f4394c84168cf183638f493ca6396550c4071
Max Thom Stahl <max@dangerouscuteness.com>
2010-04-30 13:14:10 -0500

    Added /main redirect á la Chatterböxen.

--- application.rb
+++ application.rb
@@ -6,6 +6,10 @@ require 'haml'
 
 # Dir[File.dirname(__FILE__) + '/lib/*.rb'].each {|file| require file }
 
+get "/" do
+  redirect "/main"
+end
+
 get "/:room" do
   @room = params[:room]
   haml :home
